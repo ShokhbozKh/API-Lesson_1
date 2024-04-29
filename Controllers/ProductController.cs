8f08db34913e309870aa3dced0e5ef2879814e5d
Shohboz Xurramov <shohboztuit@gmail.com>
2024-04-30 02:22:36 +0500

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,95 @@
+﻿using lesson1_1.Models;
+using lesson1_1.Stores;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace lesson1_1.Controllers;
+
+[Route("api/product")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    public ProductDataStore _store;
+    public ProductController()
+    {
+        _store = new ProductDataStore();
+    }
+    // GET: api/Product
+    [HttpGet]
+    public  ActionResult<IEnumerable<Product>> GetAllProducts()
+    {
+        var result = _store.GetAllProducts();
+        return Ok(result);
+    }
+
+    // GET api/Product/5
+    [HttpGet("{id}")]
+    public ActionResult<Product> GetById(int id)
+    {
+        var result = _store.GetById(id);
+
+        if(result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+
+    [HttpGet("{id}/Product")]
+
+    public ActionResult<List<Product>> GetProductId(int id)
+    {
+        return _store.GetAllProducts(id);
+    }
+
+    // POST api/Product/Post
+    [HttpPost]
+    public ActionResult<Product> Post([FromBody] Product product)
+    {
+        if(product == null)
+        {
+            return NotFound();
+        }
+        _store.Create(product);
+        return NoContent();
+    }
+
+    // PUT api/Product/5
+    [HttpPut("{id}")]
+    public ActionResult<Product> Put(int id, [FromBody] Product product)
+    {
+        if(id != product.Id)
+        {
+            return BadRequest();
+        }
+
+
+        var findIndex = _store.GetById(id);
+
+
+        if (findIndex!.Id == 0)
+        {
+            return NoContent();
+        }
+        _store.Update(product);
+        return Ok($"okey {product}");
+    }
+
+    // DELETE api/Product/5
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        var findIndex = _store.GetById(id);
+
+        if(findIndex!.Id == 0)
+        {
+            return NotFound($"content yuq{findIndex}");
+        }
+        _store.Delete(findIndex);
+
+        return NoContent();
+    }
+}
+
---
+++ Stores/ProductDataStore.cs
@@ -0,0 +1,39 @@
+﻿using lesson1_1.Models;
+
+namespace lesson1_1.Stores
+{
+    public class ProductDataStore
+    {
+        public static List<Product> products = new List<Product>()
+        {
+        new Product{Id = 1, Name="Apple", Description="bu shirin meva", Price=12341.31m},
+        new Product{Id = 2, Name="Banana", Description="bu shirin meva", Price=21212},
+        new Product{Id = 3, Name="Pineapple", Description="bu shirin meva", Price=434},
+        new Product{Id = 4, Name="Tomato", Description="bu achchiq meva", Price=123546541.31m},
+        new Product{Id = 5, Name="Potato", Description="bu bu ham meva", Price=768861.31m}
+        };
+
+        public List<Product> GetAllProducts() => products;
+
+        public List<Product> GetAllProducts(int id) => products.Where(x=>x.Id==id).ToList();
+        
+        public Product? GetById(int id)
+        {
+            var resutl = products.FirstOrDefault(x => x.Id == id);
+            return resutl == null ? null : resutl;
+        }
+        public Product Create(Product product)
+        {
+            products.Add(product);
+            return product;
+        }
+
+        public void Update(Product product)
+        {
+           
+            products.Add(product);
+        }
+        public void Delete(Product product) { products.Remove(product); }
+        
+    }
+}
